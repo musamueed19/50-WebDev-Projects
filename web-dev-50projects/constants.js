2f7325ac438d2d408d27f7e62e04aed663589513
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-05-02 10:19:48 +0500

    img chance in proj1

--- web-dev-50projects/constants.js
+++ web-dev-50projects/constants.js
@@ -1,6 +1,7 @@
 export const expandingCards = [
   {
-    url: "https://plus.unsplash.com/premium_photo-1673657332512-b87806f4f49e?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "https://images.unsplash.com/photo-1665395806066-d47f41e6aa6d?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // url: "https://plus.unsplash.com/premium_photo-1673657332512-b87806f4f49e?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Explore the World",
   },
   {
