a23bd76ff60398ca5efef8e9b927ef2d33370609
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-05-04 19:37:07 +0500

    working on proj2, added react jsx and module.css

--- web-dev-50projects/app/2-progresssteps/page.jsx
+++ web-dev-50projects/app/2-progresssteps/page.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import styles from "./style.module.css"
+
+
 const ProgressStepsPage = () => {
   return (
-    <div>ProgressStepsPage</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.progressContainer}>
+        {/* progress line */}
+        <div className={styles.progress}></div>
+
+        {/* progress steps - circles */}
+        <div className={`${styles.circle} ${styles.active}`}>1</div>
+        <div className={`${styles.circle} ${styles.active}`}>2</div>
+        <div className={styles.circle}>3</div>
+        <div className={styles.circle}>4</div>
+      </div>
+
+      {/* buttons */}
+      <button className={styles.btn} id="prev" disabled>
+        Prev
+      </button>
+      <button className={styles.btn} id="next">
+        Next
+      </button>
+    </div>
+  );
 }
 
 export default ProgressStepsPage
