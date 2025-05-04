f58a7be1aa12fdf1eae724109e18146e2d3e4012
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-05-04 20:00:26 +0500

    completed functionality of proj2, part-1

--- web-dev-50projects/app/2-progresssteps/page.jsx
+++ web-dev-50projects/app/2-progresssteps/page.jsx
@@ -1,31 +1,66 @@
-import React from 'react'
+"use client";
 
-import styles from "./style.module.css"
+import React, { useEffect, useState } from "react";
 
+import styles from "./style.module.css";
 
 const ProgressStepsPage = () => {
+  const [stepIndex, setStepIndex] = useState(1);
+  const totalSteps = 4;
+
+  // Calculate the width of the progress line
+  const progressWidth = ((stepIndex - 1) / (totalSteps - 1)) * 100;
+
+  // useEffect(() => {
+  //   console.log(stepIndex);
+  // }, [stepIndex]);
+
   return (
     <div className={styles.container}>
       <div className={styles.progressContainer}>
         {/* progress line */}
-        <div className={styles.progress}></div>
+        <div className={styles.progress} style={{
+          width: `${progressWidth}%`
+        }}></div>
 
         {/* progress steps - circles */}
-        <div className={`${styles.circle} ${styles.active}`}>1</div>
+        {/* <div className={`${styles.circle} ${styles.active}`}>1</div>
         <div className={`${styles.circle} ${styles.active}`}>2</div>
         <div className={styles.circle}>3</div>
-        <div className={styles.circle}>4</div>
+        <div className={styles.circle}>4</div> */}
+
+        {/* dynamically render progress steps - circles */}
+        {[...Array(totalSteps)].map((_, index) => (
+          <div
+            key={index + 1}
+            className={`${styles.circle} ${
+              index + 1 <= stepIndex && styles.active
+            }`}
+          >
+            {index + 1}
+          </div>
+        ))}
       </div>
 
       {/* buttons */}
-      <button className={styles.btn} id="prev" disabled>
+      <button
+        className={styles.btn}
+        id="prev"
+        disabled={stepIndex === 0}
+        onClick={() => setStepIndex((prev) => Math.max(0, --prev))}
+      >
         Prev
       </button>
-      <button className={styles.btn} id="next">
+      <button
+        className={styles.btn}
+        id="next"
+        disabled={stepIndex === 4}
+        onClick={() => setStepIndex((prev) => Math.min(totalSteps, ++prev))}
+      >
         Next
       </button>
     </div>
   );
-}
+};
 
-export default ProgressStepsPage
+export default ProgressStepsPage;
---
+++ web-dev-50projects/components/2-progresssteps/StepCircles.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StepCircles = () => {
+  return (
+    <div>StepCircles</div>
+  )
+}
+
+export default StepCircles
